6fdfd6136db37cca5f50fcc2925d67df5ac05398
Kenshin <xiyingit@gmail.com>
2015-01-27 15:50:48 +0800

    ...

--- go/entry/entry_thrift.go
+++ go/entry/entry_thrift.go
@@ -1,19 +1,20 @@
 package entry
 
 import (
-        "fmt"
 	"code.google.com/p/goprotobuf/proto"
 	. "github.com/udoyu/entry_thrift/go/entry/gen-go/entry"
-	"github.com/udoyu/utils/beego/controller"
 	"github.com/udoyu/utils/thrift/thriftst"
 )
 
 type EntryClient struct {
         st *thriftst.ThriftSt
 	client   *EntryThriftSvrClient
-	handlers map[string]func(*controller.Controller)int
-	//handlers map[string]EntryInterfaceHandler
-	//	handlers map[string]func(*controller.Controller)
+}
+
+func (this *EntryClient) Copy() *EntryClient {
+    c := &EntryClient{}
+    c.Init(this.st)
+    return c
 }
 
 func (this *EntryClient) Open() error {
@@ -29,29 +30,6 @@ func (this *EntryClient) Init(st *thriftst.ThriftSt) {
 		st.TProtocolFactory())
         this.st = st
 	this.client = client
-	//        c.handlers = make(map[string]EntryInterfaceHandler)
-	this.handlers = make(map[string]func(*controller.Controller)int)
-}
-
-func (this *EntryClient) Handler(ctl *controller.Controller) bool {
-        fmt.Println("path=" + ctl.Path())
-	f, ok := this.handlers[ctl.Path()]
-	if ok {
-		f(ctl)
-	}
-        this.DefaultHandler(ctl)
-	return ok
-}
-
-func (this *EntryClient) AddHandler(path string, h func(*controller.Controller)int) {
-	this.handlers[path] = h
-}
-
-func (this *EntryClient) DelHandler(path string) {
-	delete(this.handlers, path)
-}
-
-func (this *EntryClient) DefaultHandler(ctl *controller.Controller) {
 }
 
 func (this *EntryClient) Send(main_cmd, sub_cmd int32,
